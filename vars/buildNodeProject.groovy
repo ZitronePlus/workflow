b8cdb25e56eadcccf27c89131cf0e0793ba73c0f
ZitronePlus <tobiasscharf92@gmail.com>
2020-07-20 13:31:32 +0200

    Update buildNodeProject.groovy

--- vars/buildNodeProject.groovy
+++ vars/buildNodeProject.groovy
@@ -35,14 +35,14 @@ def call(Map pipelineParams = [:]) {
             }           
             stage('Build Docker Image') {
                 steps {                   
-                       
+                       script {
                          //git 'https://github.com/ZitronePlus/MOC.git'
                         //logger.banner(STAGE_NAME)
                         //logger.info('Docker image cleanup before release') 
                         docker.withRegistry( 'https://registry.hub.docker.com', 'dockerHub' ) {
                         dockerImage = docker.build("lemtron/MOC")
                         }
-                   
+                    }
                 }
             }
             stage('Push docker image') {
