50f4bce5395b5d0f4315e6701b2408bbf0d13220
ZitronePlus <tobiasscharf92@gmail.com>
2020-07-21 12:57:16 +0200

    Update buildJavascriptApp.groovy

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -2,10 +2,17 @@ def call(Map config=[:], Closure body) {
     node {
         git url: "https://github.com/werne2j/sample-nodejs"        
         
+        
+        stage("Clean") {
+            sh "gulp clean"
+            sh "npm clean"
+        }        
+        
         stage("Install") {
             sh "npm install"
-        }      
+        } 
         
+       
         stage("Test") {
             sh "npm test"
         }   
